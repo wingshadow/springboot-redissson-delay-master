a4ba74be4d7328c3a1804f243fd55c6accaa0669
LAPTOP-PRAN7OVE\zhangbin <zhangqiyi1@163.com>
2023-03-25 17:35:22 +0800

    1

--- src/main/java/com/hawk/admin/controller/topic/RedisTopicController.java
+++ src/main/java/com/hawk/admin/controller/topic/RedisTopicController.java
@@ -1,15 +1,11 @@
 package com.hawk.admin.controller.topic;
 
-import com.hawk.admin.queue.RedisQueueUtil;
 import com.hawk.admin.stream.RedisTopicUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @program: springboot-redissson-delay-master
@@ -19,12 +15,12 @@ import java.util.Map;
  */
 
 @RestController
-@RequestMapping(value = "/queue")
+@RequestMapping(value = "/topic")
 public class RedisTopicController {
     @Autowired
     private RedisTopicUtil redisTopicUtil;
 
-    @PostMapping(value = "/addQueue")
+    @PostMapping(value = "/add")
     public ResponseEntity addQueue() {
         for (int i = 100; i < 200; i++) {
             redisTopicUtil.add(i + "0", "EMAIL");
