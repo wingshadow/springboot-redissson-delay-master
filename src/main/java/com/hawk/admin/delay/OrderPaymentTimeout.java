8b17bdfb8bdcc90e0ca5a1260dd0eb24e631c351
LAPTOP-PRAN7OVE\zhangbin <zhangqiyi1@163.com>
2023-03-28 19:33:43 +0800

    1

--- src/main/java/com/hawk/admin/delay/OrderPaymentTimeout.java
+++ src/main/java/com/hawk/admin/delay/OrderPaymentTimeout.java
@@ -7,7 +7,14 @@ package com.hawk.admin.delay;
  * @create: 2023-03-20 10:21
  */
 
+import cn.hutool.core.exceptions.ExceptionUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.Record;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -18,10 +25,21 @@ import java.util.Map;
 @Component
 @Slf4j
 public class OrderPaymentTimeout implements RedisDelayQueueHandle<Map> {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public void execute(Map map) {
-        log.info("map:{}", map);
-        // TODO 订单支付超时，自动取消订单处理业务...
-
+        try{
+//             创建消息记录, 以及指定stream
+            ObjectRecord<String, String> stringRecord =
+                    Record.of(JSONObject.toJSONString(map)).withStreamKey("mystream");
+            // 将消息添加至消息队列中
+            this.stringRedisTemplate.opsForStream().add(stringRecord);
+//            log.info("{}", JSON.toJSONString(map));
+        }catch (Exception e){
+            log.info("pubClassRecord error:{}", ExceptionUtil.stacktraceToString(e));
+        }
     }
 }
